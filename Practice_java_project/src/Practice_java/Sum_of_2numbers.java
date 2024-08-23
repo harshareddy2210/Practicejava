f4b61929055635c9ae0cca5415bce08a84bbfd3a
2125318 <2125318@LTIN341043.cts.com>
2024-08-23 13:41:15 +0530

    Adding sum code

---
+++ Practice_java_project/src/Practice_java/Sum_of_2numbers.java
@@ -0,0 +1,14 @@
+package Practice_java;
+import java.util.*;
+
+public class Sum_of_2numbers {
+	public static void main(String[] args) {
+		Scanner number = new Scanner (System.in);
+	    System.out.println("enter the number");
+	    int number1 = number.nextInt();
+	    System.out.println("enter the number 2");
+	    int number2 = number.nextInt();
+	    int sum = number1 + number2;
+	    System.out.println(sum);
+	}
+}
