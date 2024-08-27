eda2738f77c1fbd8f6cc23b8d2258e60232ceed6
2125318 <2125318@LTIN341043.cts.com>
2024-08-27 12:50:02 +0530

    if else and relational operator

---
+++ Practice_java_project/src/Practice_java/Relational_Operators.java
@@ -0,0 +1,10 @@
+package Practice_java;
+public class Relational_Operators {
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 6;
+		boolean result = x<y;
+		System.out.println(result);
+	}
+
+}
